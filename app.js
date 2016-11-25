53161f779e553747b3ff9024bff65263a79463a4
Lee Broxson <broxsonl@gmail.com>
2016-11-24 22:03:09 -0800

    Created a trivia module for the array and moved it into its own module.

---
+++ app.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const triviaArray = require('./trivia.js');
+
+console.log(triviaArray[0].text);
---
+++ trivia.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = [
+  {
+    item: '1',
+    text: 'I hear it\'s amazing when the purple-stuffed worm in flap-jaw space does a raw blink on harakiri rock.',
+  },
+  {
+    item: '2',
+    text: 'Solid Snake is Liquid Snake\'s brother.',
+  },
+  {
+    item: '3',
+    text: 'Luffy WILL become the next pirate king.',
+  },
+  {
+    item: '4',
+    text: 'Rich Evans is a phenomenal crank phone caller.',
+  },
+  {
+    item: '5',
+    text: 'Frankie Manning is the Ambassador of Lindy Hop',
+  },
+  {
+    item: '6',
+    text: 'The number 64 is larger than 63.',
+  },
+  {
+    item: '7',
+    text: 'These men are pawns!',
+  },
+];
